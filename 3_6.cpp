1d6ae587bfaa3c902435a547b8b662219cfe05fe
liuchen5214 <32033443+liuchen5214@users.noreply.github.com>
2017-10-15 19:56:54 +0800

    Add files via upload

---
+++ 3_6.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+void main()
+{
+	float r,k,c,s;
+	cout<<"r k"<<endl;
+	cin>>r>>k;
+	s=3.14159*r*r;
+	c=2*3.14159*r;
+	if(k==1)
+		cout<<s<<endl;
+	else
+		if(k==2)
+			cout<<c<<endl;
+		else
+			if(k==3)
+			cout<<s<<c<<endl;
+
+}
