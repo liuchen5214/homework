954dbb4fd4565a5893890ccff4c21777ee0008a2
liuchen5214 <32033443+liuchen5214@users.noreply.github.com>
2017-10-15 19:51:33 +0800

    Add files via upload

---
+++ 3_3.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+void main()
+{#include <iostream.h>
+void main()
+{
+	int x,y;
+	cout<<"x:"<<endl;
+		cin>>x;
+	if(x%3==0)
+		y=1;
+	else
+	   if(x%7==0)
+		   y=1;
+	   else
+		   y=0;
+	if(y==1)
+		cout<<x<<":YES.\n";
+	else
+		cout<<y<<":NO.\n";
+}
